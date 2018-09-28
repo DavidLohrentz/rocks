c7128df0eb9589d5f1fdbe23d1a802c05c79a2ed
David Lohrentz <david_lohrentz@yahoo.com>
2018-09-27 19:18:24 -0500

    removed the volume code from startup

--- start_rock.sh
+++ start_rock.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-docker run --rm -ti -p 4000:80 \
--v /home/david/projects/rocks:/var/opt \
-rocks
+# docker run --rm -ti -p 4000:80 \
+# -v /home/david/projects/rocks:/var/opt \
+# rocks
+
+docker run --rm -ti rocks
